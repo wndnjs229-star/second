854e8c298d1c6ad74d14b19c55250519c31da34d
wndnjs229 <wndnjs229@gmail.com>
2026-04-30 11:10:16 +0900

    추가로 작업물

--- js/test.js
+++ js/test.js
@@ -1,36 +1,14 @@
-// const result = document.getElementById("result");
-// const hours = new Date().getHours();
+const result = document.getElementById("result");
 
-// if (hours >= 13 && hourts < 17) {
-//   result.textContent = "안녕";
-//   result.className = "daytime";
-// } else {
-//   result.textContent = "잠잔다";
-//   result.className = "nighttime";
-// }
-
-function add(a, b) {
+function oldAdd(a, b) {
   return a + b;
 }
 
-let rec = add(3, 6);
-// 여기까지 함수 원형
-
-// 1.
-const add = function (a, b) {
-  return a + b;
-};
-
-// 2.
-const add = (a, b) => {
-  return a + b;
-};
-
-// 3.
-const add = (a, b) => a + b;
-
-const sayHello = () => console.log("welcome");
-
-// 매개변수가 같을 때에는 소괄호 까지도 생략이 됩니다.
-const double = (x) => x * 2;
-const double = (x) => x * 2;
+const newAdd = (a, b) => a + b;
+// ` ` 백틱은 문자의 열과 자바스크립트의 문자를 연결시킬 때 사용한다.
+// 자바스크립트에서 사용하고, html과 연결시킬때 사용한다.
+// 변수가 들어가던 안들어가던 백틱 사용은 무조건 필요로 한다.
+result.innerHTML = `
+<p> 기존함수: ${oldAdd(5, 3)} </p>
+<p> 새로운 함수: ${newAdd(5, 3)} </p>
+`;
