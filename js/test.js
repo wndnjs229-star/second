604303c98806319bb8ad595f9bd65e8d95cba434
wndnjs229 <wndnjs229@gmail.com>
2026-04-30 10:17:04 +0900

    0429 오전 첫번째 수업

--- js/test.js
+++ js/test.js
@@ -1,17 +1,10 @@
-// 키보드 입력 방식을 반드시 알아야 한다.
-// prompt()
+const result = document.getElementById("result");
+const hours = new Date().getHours();
 
-// 1부터 사용자로부터 입력받은 정수까지의 정수의 합계를 입력하시오
-
-while (1) {
-  let input = parseInt(prompt("정수 입력하시오!!!"));
-  if (input === 7) {
-    console.log("7이 입력되었으니 종료합니다");
-    break;
-  }
-  let tot = 0;
-  for (i = 1; i <= input; i++) {
-    tot = tot + i;
-  }
-  comsole.log(tot);
+if (hours >= 13 && hourts < 17) {
+  result.textContent = "안녕";
+  result.className = "daytime";
+} else {
+  result.textContent = "잠잔다";
+  result.className = "nighttime";
 }
